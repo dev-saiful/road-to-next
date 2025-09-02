0ed44a71dd15a26162cd45ce6af54570cd4d16d1
dev-saiful <saifulcseian@gmail.com>
2025-09-03 00:38:31 +0600

    dev:data fetch from db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,33 @@
-export const initialTickets = [
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const tickets: Prisma.TicketCreateInput[] = [
   {
-    id: "1",
     title: "Ticket 1",
     description:
       "Description for Ticket 1 lorem ipsum dolor sit amet. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
     status: "DONE" as const,
   },
   {
-    id: "2",
     title: "Ticket 2",
     description:
       "Description for Ticket 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     status: "OPEN" as const,
   },
   {
-    id: "3",
     title: "Ticket 3",
     description:
       "Description for Ticket 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     status: "IN_PROGRESS" as const,
   },
 ];
+
+const seed = async () => {
+  await prisma.ticket.deleteMany();
+  await prisma.ticket.createMany({
+    data: tickets,
+  });
+};
+
+seed();
